1a117323e596c0139c75f02bd0a577e65862b359
datmcl <mixail.bokov9@gmail.com>
2021-08-07 10:46:13 +0700

    add interface

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dateTextField.placeholder = "Enter the date"
+        monthTextField.placeholder = "Enter the month"
+        yearTextField.placeholder = "Enter the year"
     }
 
-
+    @IBAction func findDay(_ sender: UIButton) {
+    }
+    
 }
 
