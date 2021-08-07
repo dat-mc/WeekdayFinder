fe445065cf0a5ab9d247b3c0049732e9fcd32733
datmcl <mixail.bokov9@gmail.com>
2021-08-08 04:12:18 +0700

    update ui

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -14,16 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var yearTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        dateTextField.placeholder = "Enter the date"
-        monthTextField.placeholder = "Enter the month"
-        yearTextField.placeholder = "Enter the year"
     }
 
     @IBAction func findDay(_ sender: UIButton) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
